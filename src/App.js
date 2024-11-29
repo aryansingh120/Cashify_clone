c1276bd63f6ed8beccf38d6c987e2c5817ed0f44
aryansingh120 <aryanhp940@gmail.com>
2024-11-29 13:58:09 +0530

    star img changed

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
       <Navbar/>
       <Services/>
       <Stores/>
-      <Questions/>
-      <Article/>
-      <Footer/> 
+      {/* <Questions/> */}
+      {/* <Article/> */}
+      {/* <Footer/>  */}
      
      
     </div>
