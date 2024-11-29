97cb45592952f26892ef6dbb50cd46481395e0c3
aryansingh120 <aryanhp940@gmail.com>
2024-11-29 12:55:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Navbar";
+import Services from "./Components/Services";
+import Stores from "./Components/Stores"
+import Questions from "./Components/Questions"
+import Article from "./Components/Article"
+import Footer from "./Components/Footer"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Navbar/>
+      <Services/>
+      <Stores/>
+      <Questions/>
+      <Article/>
+      <Footer/> 
+     
+     
     </div>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+
+const Footer = () => {
+  let imgArr=["/photos/twitter.webp","/photos/fb.webp","/photos/insta.webp","/photos/youtube.webp"];
+  let textArr=[["Sell Phone","Sell Television","Sell Smart Watch","Sell Smart Speakers","Sell DSLR Camera","Sell Earbuds","Repair Phone","Buy Phone","Recycle Phone","Finr New Phone","Partner With Us"],["About us","Careers","Articles","Press Releses","Become cashify Partner","Become supersale partner"],["Mobile Phone","laptop","tablet","imac","gaming consoles"],["FAq","contact us","Warrenty policy","refund policy"],["Terms & conditions","privacy policy","terms of use","e-waste policy","coolie policy","gdpr compliance","what is refurbished","device safty"]]
+  return (
+    <div className='bg-[#F7F7F7] px-[2vw]'>
+        <div className='sm:flex sm:gap-[2.5rem] '>
+            
+            <div className='sm:flex '>
+             
+              <div className=' sm:w-[20rem] sm:flex-shrink-0 '>
+              <div className=' h-[5rem] overflow-hidden '>
+              <img src="/photos/cashify3-remove.png" alt="img not available" className=' h-[10rem] mt-[-2rem] flex-shrink-0'  />
+              </div>
+              <div className='flex pl-[.5rem] sm:flex-col'>
+                <p className='mr-[1rem] mt-[.2rem] flex-shrink-0 text-[#707070] font-bold'>Follow us on</p>
+
+                <div className=' flex gap-[1rem] sm:mt-[1rem] '>
+                  {
+                     imgArr.map((item)=>{
+                      return <img src={item} alt="img not available" className=' h-[2rem]' />
+                    })
+                  }
+                </div>
+              </div>
+
+              </div>
+              <div className='border-b-2 sm:border-none py-[1rem]'>
+                   <p className='text-[#707070] font-bold ml-[.5rem] mb-[1rem]'>Services</p>
+                   <ul>
+                   {
+                    textArr[0].map((item)=>{
+                      return <li className=' capitalize text-[#707070] font-medium text-[13px] mt-[10px] ml-[1.1rem]'>{item}</li>
+                    })
+                   }
+                   </ul>
+            </div>
+
+            </div>
+
+            <div className='border-b-2 sm:border-none py-[1rem]'>
+            <p className='text-[#707070] font-bold mb-[1rem] ml-[.5rem]'>Company</p>
+                   <ul>
+                   {
+                    textArr[1].map((item)=>{
+                      return <li className=' capitalize text-[#707070] font-medium ml-[1.1rem] text-[13px] mt-[10px]'>{item}</li>
+                    })
+                   }
+                   </ul>
+            </div>
+            <div className='border-b-2 sm:border-none  py-[1rem]'>
+            <p className='text-[#707070] font-bold mb-[1rem] ml-[.5rem]'>Sell Device</p>
+                   <ul>
+                   {
+                    textArr[2].map((item)=>{
+                      return <li className=' capitalize text-[#707070] font-medium ml-[1.1rem] text-[13px] mt-[10px]'>{item}</li>
+                    })
+                   }
+                   </ul>
+            </div>
+            <div className='border-b-2 sm:border-none py-[1rem]'>
+            <p className='text-[#707070] font-bold mb-[1rem] ml-[.5rem]'>Help & Support</p>
+                   <ul>
+                   {
+                    textArr[3].map((item)=>{
+                      return <li className=' capitalize text-[#707070] ml-[1.1rem] font-medium text-[13px] mt-[10px]'>{item}</li>
+                    })
+                   }
+                   </ul>
+            </div>
+            <div className='border-b-2 sm:border-none  py-[1rem] '>
+            <p className='text-[#707070] font-bold mb-[1rem] ml-[.5rem]'>More Info</p>
+                   <ul>
+                   {
+                    textArr[4].map((item)=>{
+                      return <li className=' capitalize text-[#707070] ml-[1.1rem] font-medium text-[13px] mt-[10px]'>{item}</li>
+                    })
+                   }
+                   </ul>
+                   <button className='bg-[#42C8B7] flex rounded-xl p-[.5rem] mt-[1rem]'>
+                    <img src="/photos/chat.svg" alt="img not available" className='h-[2.5rem] mt-[.3rem] mr-[.5rem]' />
+                    <p className='text-white text-start'>
+                      <p className='font-bold'>Chat with Us</p>
+                      <p className='font-medium text-[12px] mt-[10px]'>Got questions? Just ask.</p>
+                    </p>
+                   </button>
+            </div>
+        </div>
+        {/* ****************************************************************** */}
+          <div className='flex flex-col gap-[2rem] items-center sm:flex-row sm:border-y-2 py-[1rem]'>
+          <div className='pl-[1rem] sm:w-[70%] sm:text-[12px] font-medium'>
+            <p className='text-[#707070]'>Registered Office:</p>
+            <p className=''>Manak Waste Management Pvt Ltd.| B-39, 1st Floor, Middle Circle, Connaught Place, New Delhi-110001, India, Support-7290068900 | CIN: U27205DL2009PTC190441 </p>
+            <p className=''>Manak Waste Management Pvt Ltd. is ISO 27001 & 27701 Compliance Certified. Person who may be contacted in case of any compliance related queries or grievances : Manoj Kumar </p>
+            <p className='cursor-pointer text-[#42C8B7] font-medium'>(sarmendersingh20@gmail.com)</p>
+            <p className='mt-[1rem]'>** All product names, logos, and brands are property of their respective owners. All company, product and service names used in this website are for identification purposes only. Use of these names, logos, and brands does not imply endorsement.</p>
+          </div>
+          <div className='w-[80%] sm:w-[30%]'>
+            <img src="/photos/btn3.svg" alt="img not available" className=''/>
+          </div>
+          </div>
+          <p className='text-[12px] font-medium my-[2rem] ml-[1rem]'>Copyright @ 2024 Cashify All rights reserved</p>
+      
+    </div>
+  )
+}
+
+export default Footer
